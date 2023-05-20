2ab23065e7a21158321065f9bc0ad3da998e82c6
Noah Friedman <noahfriedman2@gmail.com>
2023-05-20 13:58:47 -0400

    Fix Copilot toggle touchbar button

--- nvim/lua/touchbar.lua
+++ nvim/lua/touchbar.lua
@@ -5,7 +5,7 @@ local M = {
 		{ label = 'Diagnostic', command = "lua vim.diagnostic.open_float()"},
 		{ label = 'Clear Highlight', command = "noh"},
 		{ label = 'Go to Definition', command = "lua vim.lsp.buf.definition()" },
-		{ label = 'Copilot: On', command = "lua require'touchbar'.toggleCopilot()"},
+		{ label = 'Copilot: On', command = "lua require'dotfiles.touchbar'.toggleCopilot()"},
 	}
 }
 
@@ -34,9 +34,9 @@ end
 function M.toggleCopilot()
 	vim.b.copilot_enabled = vim.fn['copilot#Enabled']() == 0
 	if vim.b.copilot_enabled then
-		M.mappings[5].label = 'Copilot: On'
+		M.mappings[6].label = 'Copilot: On'
 	else
-		M.mappings[5].label = 'Copilot: Off'
+		M.mappings[6].label = 'Copilot: Off'
 	end
 	vim.cmd "call it2touchbar#RegenKeys()"
 end
