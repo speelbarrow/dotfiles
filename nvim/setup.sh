ee31f98b6b1a41d7173273f930649e74457924e4
Noah Friedman <noahfriedman2@gmail.com>
2023-05-31 12:35:46 -0400

    Move nvim/lua directory to nvim/lua/dotfiles

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -9,5 +9,5 @@ file ~/.config/nvim
 # Create symlinks for Neovim config files
 mkdir -p ~/.config/nvim/lua
 ln -s "$(dirname "$0")/init.lua" ~/.config/nvim/init.lua
-ln -s "$(dirname "$0")/lua" ~/.config/nvim/lua/dotfiles
+ln -s "$(dirname "$0")/lua/dotfiles" ~/.config/nvim/lua/dotfiles
 echo "Created Neovim config symlink"
