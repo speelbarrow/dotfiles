82e2626223451de1e921fb0a18c9cb112f34344d
Noah Friedman <noahfriedman2@gmail.com>
2023-06-01 14:53:35 -0400

    Fix Copilot key toggle

--- nvim/lua/dotfiles/setup/vim-it2-touchbar.lua
+++ nvim/lua/dotfiles/setup/vim-it2-touchbar.lua
@@ -14,21 +14,20 @@ function M.touchbar()
 	end
 end
 
--- Listeners for BufLeave and CopilotToggled events to switch label text
+-- Helper function for updating Copilot key label
+local function update_copilot_key()
+    keymap.function_keys[#keymap.function_keys][1] = (vim.fn["copilot#Enabled"]() ~= 0) and "Copilot: On"
+                                                        or "Copilot: Off"
+    vim.fn["it2touchbar#RegenKeys"]()
+end
 
-vim.api.nvim_create_autocmd("BufEnter", {
+-- Don't enable the `BufEnter` autocommand until the `UIEnter` event has fired
+vim.api.nvim_create_autocmd("UIEnter", {
 	callback = function()
-		keymap.function_keys[#keymap.function_keys][1] = (vim.fn["copilot#Enabled"]() ~= 0) and "Copilot: On" or "Copilot: Off"
-		vim.fn["it2touchbar#RegenKeys"]()
+        vim.api.nvim_create_autocmd("BufEnter", { callback = update_copilot_key })
 	end
 })
 
-vim.api.nvim_create_autocmd("User", {
-	pattern = "CopilotToggled",
-	callback = function()
-		keymap.function_keys[#keymap.function_keys][1] = (vim.fn["copilot#Enabled"]() ~= 0) and "Copilot: On" or "Copilot: Off"
-		vim.fn["it2touchbar#RegenKeys"]()
-	end
-})
+vim.api.nvim_create_autocmd("User", { pattern = "CopilotToggled", callback = update_copilot_key })
 
 return M
