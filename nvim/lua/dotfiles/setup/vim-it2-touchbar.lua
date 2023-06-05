1fd886e3c6d228bf0822e27066b859cf3b7fe7c5
Noah Friedman <noahfriedman2@gmail.com>
2023-06-05 12:56:32 -0400

    Update touchbar config to use nvim scheduling

--- nvim/lua/dotfiles/setup/vim-it2-touchbar.lua
+++ nvim/lua/dotfiles/setup/vim-it2-touchbar.lua
@@ -18,15 +18,11 @@ end
 local function update_copilot_key()
     keymap.function_keys[#keymap.function_keys][1] = (vim.fn["copilot#Enabled"]() ~= 0) and "Copilot: On"
                                                         or "Copilot: Off"
-    vim.schedule_wrap(function() vim.fn["it2touchbar#RegenKeys"]() end)
+    vim.schedule_wrap(vim.fn["it2touchbar#RegenKeys"])()
 end
 
 -- Don't enable the `BufEnter` autocommand until the `UIEnter` event has fired
-vim.api.nvim_create_autocmd("UIEnter", {
-	callback = function()
-        vim.api.nvim_create_autocmd("BufEnter", { callback = update_copilot_key })
-	end
-})
+vim.api.nvim_create_autocmd("BufEnter", { callback = update_copilot_key })
 
 vim.api.nvim_create_autocmd("User", { pattern = "CopilotToggled", callback = update_copilot_key })
 
