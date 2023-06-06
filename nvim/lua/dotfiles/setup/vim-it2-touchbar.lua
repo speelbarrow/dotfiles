ce155c908cf130dc20ca69a3d0e19176f9ca4fbe
Noah Friedman <noahfriedman2@gmail.com>
2023-06-06 12:44:13 -0400

    Add more scheduling for vim-it2-touchbar calls

--- nvim/lua/dotfiles/setup/vim-it2-touchbar.lua
+++ nvim/lua/dotfiles/setup/vim-it2-touchbar.lua
@@ -10,15 +10,17 @@ endfunction
 
 function M.touchbar()
 	for index = 1, 24 do
-		vim.cmd("TouchBarLabel F"..index.." '"..(keymap.function_keys[index] or { " " })[1].."'")
+		vim.schedule(function()
+            vim.cmd("TouchBarLabel F"..index.." '"..(keymap.function_keys[index] or { " " })[1].."'")
+        end)
 	end
 end
 
 -- Helper function for updating Copilot key label
 local function update_copilot_key()
     keymap.function_keys[#keymap.function_keys][1] = (vim.fn["copilot#Enabled"]() ~= 0) and "Copilot: On"
-                                                        or "Copilot: Off"
-    vim.schedule_wrap(vim.fn["it2touchbar#RegenKeys"])()
+    or "Copilot: Off"
+    vim.schedule(vim.fn["it2touchbar#RegenKeys"])
 end
 
 -- Don't enable the `BufEnter` autocommand until the `UIEnter` event has fired
