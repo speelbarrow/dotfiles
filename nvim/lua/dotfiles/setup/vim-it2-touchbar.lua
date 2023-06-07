cf51168244a2245e1735dbf397ae733a0ec9b919
Noah Friedman <noahfriedman2@gmail.com>
2023-06-07 10:14:37 -0400

    Further updates to vim-it2-touchbar scheduling

--- nvim/lua/dotfiles/setup/vim-it2-touchbar.lua
+++ nvim/lua/dotfiles/setup/vim-it2-touchbar.lua
@@ -4,28 +4,31 @@ local M = {}
 
 vim.cmd [[
 function! TouchBar() 
-	lua require'dotfiles.setup.vim-it2-touchbar'.touchbar() 
+lua require'dotfiles.setup.vim-it2-touchbar'.touchbar() 
 endfunction
 ]]
 
 function M.touchbar()
-	for index = 1, 24 do
-		vim.schedule(function()
-            vim.cmd("TouchBarLabel F"..index.." '"..(keymap.function_keys[index] or { " " })[1].."'")
-        end)
-	end
+    for index = 1, 24 do
+        vim.cmd("TouchBarLabel F"..index.." '"..(keymap.function_keys[index] or { " " })[1].."'")
+    end
 end
 
 -- Helper function for updating Copilot key label
 local function update_copilot_key()
     keymap.function_keys[#keymap.function_keys][1] = (vim.fn["copilot#Enabled"]() ~= 0) and "Copilot: On"
     or "Copilot: Off"
-    vim.schedule(vim.fn["it2touchbar#RegenKeys"])
+
+    vim.fn["it2touchbar#RegenKeys"]()
 end
 
--- Don't enable the `BufEnter` autocommand until the `UIEnter` event has fired
-vim.api.nvim_create_autocmd("BufEnter", { callback = update_copilot_key })
+vim.api.nvim_create_autocmd("BufEnter", { callback = vim.schedule_wrap(update_copilot_key) })
 
-vim.api.nvim_create_autocmd("User", { pattern = "CopilotToggled", callback = update_copilot_key })
+vim.api.nvim_create_autocmd("UIEnter", {
+    once = true,
+    callback = function()
+        vim.api.nvim_create_autocmd("User", { pattern = "CopilotToggled", callback = update_copilot_key })
+    end
+})
 
 return M
