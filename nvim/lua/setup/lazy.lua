794c29729b66e52d547a042a6a3d9a9410c4530b
Noah Friedman <noahfriedman2@gmail.com>
2023-06-08 17:22:55 -0400

    Switch back to original nvim-cmp repository since PR has been merged

--- nvim/lua/setup/lazy.lua
+++ nvim/lua/setup/lazy.lua
@@ -153,8 +153,7 @@ require 'lazy'.setup {
 
 		-- Provides autocompletion
 		{
-			'speelbarrow/nvim-cmp',
-			branch = 'improve-mapping-types',
+			'hrsh7th/nvim-cmp',
 			dependencies = {
 				'hrsh7th/cmp-nvim-lsp',
 				'hrsh7th/cmp-buffer',
