5555c68cfb86e5672c377256a11fc83354385c55
Noah Friedman <noahfriedman2@gmail.com>
2023-06-11 14:38:55 -0400

    Add Cmd-key mappings for Neovide

--- nvim/plugin/neovide.lua
+++ nvim/plugin/neovide.lua
@@ -11,6 +11,9 @@ if vim.g.neovide then
         vim.g["neovide_"..key] = value
     end
 
+    for key, command in pairs({ c = "y", v = "p", x = "x" }) do
+        vim.keymap.set({'n', 'i', 'v'}, "<D-"..key..">", "<Cmd>normal \"*"..command.."<CR>", { noremap = true })
+    end
 else
     vim.o.guicursor = vim.o.guicursor .. ",i:-blinkwait175-blinkoff150-blinkon175"
 end
