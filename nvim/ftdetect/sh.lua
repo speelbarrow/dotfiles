4f271ab351be09851279d50638c7adacf1b45934
Noah Friedman <noahfriedman2@gmail.com>
2023-06-11 10:52:07 -0400

    Update script to detect bash shebangs to detect zsh shebangs as well

--- nvim/ftdetect/sh.lua
+++ nvim/ftdetect/sh.lua
@@ -3,12 +3,15 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     callback = function(args)
         local first_line = vim.api.nvim_buf_get_lines(args.buf, 0, 1, false)[1]
 
-        for _, regex in ipairs({ "^#!", "bash$" }) do
-            if first_line:find(regex) == nil then
+        if first_line:find("^#!") == nil then
+            return
+        end
+
+        for _, shell in ipairs({ "bash", "zsh" }) do
+            if first_line:find(shell) ~= nil then
+                vim.bo[args.buf].filetype = shell
                 return
             end
         end
-
-        vim.bo.filetype = "bash"
     end
 })
