48bd8ee8245d17bce716cb138ec4f5a03f62b265
Noah Friedman <noahfriedman2@gmail.com>
2023-06-13 09:54:12 -0400

    Remove override of Telescope action of project.nvim extension

--- nvim/lua/setup/project_nvim.lua
+++ nvim/lua/setup/project_nvim.lua
@@ -7,20 +7,6 @@ return {
             }
         }
 
-        -- Load telescope plugin, override default action
-        local telescope = require'telescope'
-        telescope.load_extension "projects"
-
-        telescope.extensions.projects._projects = telescope.extensions.projects.projects
-        telescope.extensions.projects.projects = function(_)
-            telescope.extensions.projects._projects {
-                attach_mappings = function(bufnr, _)
-                    require'telescope.actions'.select_default:replace(function()
-                        vim.fn.maparg("w", "n", nil, true).callback(bufnr) ---@diagnostic disable-line
-                    end)
-                    return true
-                end
-            }
-        end
+        require'telescope'.load_extension "projects"
     end
 }
