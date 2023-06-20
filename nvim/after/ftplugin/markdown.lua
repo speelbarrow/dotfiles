e5674a4e57577c166bcfb3f293fa3d921f46b761
Noah Friedman <noahfriedman2@gmail.com>
2023-06-20 09:52:28 -0400

    Map 'run' action for Markdown files to launch MarkdownPreview

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,2 +1,7 @@
 vim.bo.tabstop = 2
 vim.bo.shiftwidth = 2
+
+-- Use MarkdownPreview plugin with Dispatch action system
+require'setup.dispatch'.configure_buffer {
+    run = function() vim.cmd "MarkdownPreview" end
+}
