e0cb78f4908891ef7b0c28c87bd03aeab3a80376
Noah Friedman <noahfriedman2@gmail.com>
2023-09-13 19:28:11 -0400

    Fix touchbar draw issue

--- nvim/lua/setup/lazy.lua
+++ nvim/lua/setup/lazy.lua
@@ -49,6 +49,16 @@ require 'lazy'.setup {
 			priority = 1,
 		},
 
+        {
+            "nvim-neo-tree/neo-tree.nvim",
+            branch = "v3.x",
+            dependencies = {
+                "nvim-lua/plenary.nvim",
+                "nvim-tree/nvim-web-devicons",
+                "MunifTanjim/nui.nvim",
+            },
+        },
+
         -- Git integration
         {
             'tpope/vim-fugitive',
--- nvim/lua/setup/vim-it2-touchbar.lua
+++ nvim/lua/setup/vim-it2-touchbar.lua
@@ -20,23 +20,26 @@ end
 -- Helper function for updating Copilot key label
 local function update_copilot_key()
     keylabels[#keylabels] = (vim.fn["copilot#Enabled"]() ~= 0) and "Copilot: On" or "Copilot: Off"
-    vim.cmd.sleep("10m")
-    vim.schedule(vim.fn["it2touchbar#RegenKeys"])
+    vim.fn["it2touchbar#RegenKeys"]()
 end
 
 function M.setup()
     vim.cmd [[
     function! TouchBar() 
-        lua require'setup.vim-it2-touchbar'.touchbar() 
+    lua require'setup.vim-it2-touchbar'.touchbar() 
     endfunction
     ]]
-
-    vim.api.nvim_create_autocmd("BufEnter", { callback = vim.schedule_wrap(update_copilot_key) })
 
     vim.api.nvim_create_autocmd("UIEnter", {
         once = true,
         callback = function()
-            vim.api.nvim_create_autocmd("User", { pattern = "CopilotToggled", callback = vim.schedule_wrap(update_copilot_key) })
+            update_copilot_key()
+            vim.cmd.mode()
+
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "CopilotToggled",
+                callback = update_copilot_key
+            })
         end
     })
 end
