a03c939b11d778dcab9db9b06bb28f65f22ba21a
Noah Friedman <noahfriedman2@gmail.com>
2023-09-13 09:39:12 -0400

    Improve Dispatch commands for .(t|j)s(x?) files

--- nvim/after/ftplugin/typescript.lua
+++ nvim/after/ftplugin/typescript.lua
@@ -1,6 +1,12 @@
 require'setup.dispatch'.configure_buffer({
     compiler = "bun",
-    run = function() vim.cmd("Start -wait=always bun run %") end,
+    run = function()
+        if vim.fn.filereadable("./package.json") == 1 then
+            vim.cmd("Start -wait=always bun start")
+        else
+            vim.cmd("Start -wait=always bun run %")
+        end
+    end,
     test = true,
     build = true,
 })
