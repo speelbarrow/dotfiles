e40aa89ebb8e1e9826573becff5f39ceb1ae089b
Noah Friedman <noahfriedman2@gmail.com>
2023-10-03 10:38:48 -0400

    Fix sizing for Neotree
    Remove redundant code, replace with function call in Neotree config

--- nvim/lua/setup/neo-tree.lua
+++ nvim/lua/setup/neo-tree.lua
@@ -2,7 +2,7 @@ local M = {}
 
 ---@return integer
 local function calculate_width()
-    return math.min(50, vim.o.co - vim.bo.textwidth - 6)
+    return math.min(50, vim.o.co - vim.bo.textwidth - 7)
 end
 
 function M.setup()
@@ -89,7 +89,7 @@ function M.setup()
             }
         },
         window = {
-            width = math.min(50, vim.o.columns - vim.o.textwidth - 5),
+            width = calculate_width(),
             mappings = {
                 ["<Tab>"] = function()
                     vim.cmd.wincmd 'w'
