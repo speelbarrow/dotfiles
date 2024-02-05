ea0e0f7545f968ee0a96d54ee498c8981014030f
Noah Friedman <noahfriedman2@gmail.com>
2024-02-05 09:50:00 -0500

    Fix python debug action in Neovim config

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -2,5 +2,5 @@ vim.bo.textwidth = 79
 
 require'setup.dispatch'.configure_buffer {
     run = function() vim.cmd "Start -wait=always python3 %" end,
-    debug = "python3 -m pdb %",
+    debug = function() vim.cmd "Start python3 -m pdb %" end,
 }
