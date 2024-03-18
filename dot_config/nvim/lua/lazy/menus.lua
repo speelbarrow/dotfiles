76dbd44c7c4eb3cf6f11cae45eff6f0594d2c404
Noah Friedman <noahfriedman2@gmail.com>
2024-03-18 02:40:37 +0000

    Remove .config/nvim/lua/lazy/menus.lua Remove .config/nvim/plugin/keymap.lua Remove .config/nvim/plugin/opts.lua

--- dot_config/nvim/lua/lazy/menus.lua
+++
@@ -1,9 +0,0 @@
-return --[[ @as LazySpec ]] {
-    {
-        "nvim-telescope/telescope.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim"
-        },
-        priority = 1000,
-    },
-}
