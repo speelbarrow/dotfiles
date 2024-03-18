f23a3a01feea62b6a8c3557b504cea314210871f
Noah Friedman <noahfriedman2@gmail.com>
2024-03-18 02:39:16 +0000

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/lazy/lsp.lua

--- dot_config/nvim/lua/lazy/lsp.lua
+++ dot_config/nvim/lua/lazy/lsp.lua
@@ -3,19 +3,29 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
             require"lsp"
-        end
+        end,
+        priority = 400,
     },
 
     -- Auto-completion
     {
         "ms-jpq/coq.nvim",
-        event = "LspAttach",
+        lazy = true,
         init = function()
             vim.g.coq_settings = { auto_start = true }
         end,
         build = function()
             vim.cmd "COQdeps"
         end,
+        -- config runs in `util.configure_lsp`
+    },
+
+    -- Copilot
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function() require"config.copilot".setup() end
     },
 
     -- Extension to LuaLS that makes it aware of Neovim runtime files
