36db7eee8899b535220aa202868ceba55f834105
Noah Friedman <noahfriedman2@gmail.com>
2024-03-25 19:54:43 -0400

    modified:   .config/yadm/bootstrap.d/cargo.sh

--- .config/yadm/bootstrap.d/cargo.sh
+++ .config/yadm/bootstrap.d/cargo.sh
@@ -4,7 +4,7 @@
 
 if cmd_exists cc; then
     if ! cmd_exists cargo; then
-        curl https://sh.rustup.rs -sSf | sh -s -- -y
+        curl https://sh.rustup.rs -sSf | sh -s -- -y --profile=minimal
         . "$HOME/.cargo/env"
     fi
 else
