df5f8214d3b96c8f5bcd8da938911b34aee73bcb
Noah Friedman <noahfriedman2@gmail.com>
2024-03-26 09:28:37 -0400

    modified:   lua/config/gitsigns.lua

--- .config/nvim/lua/config/gitsigns.lua
+++ .config/nvim/lua/config/gitsigns.lua
@@ -1,8 +1,18 @@
 local M = {}
 
 function M.setup()
-    require"gitsigns".setup {
+    local gitsigns = require"gitsigns"
+
+    gitsigns.setup {
+        ---@param bufnr number
+        on_attach = function(bufnr)
+            if vim.b[bufnr].gitsigns_status_dict.gitdir == vim.fn.stdpath("data")--[[ @as string ]]:gsub("/nvim",
+                "").."/yadm/repo.git" and vim.fn["fugitive#Head"]() == "" then
+                vim.schedule(function() gitsigns.detach(bufnr) end)
+            end
+        end,
         _signs_staged_enable = true,
+        attach_to_untracked = true,
         numhl = true,
         preview_config = {
             border = "rounded",
