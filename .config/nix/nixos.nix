54ca86c541ffdec0cb202d099dd426898fea5dfd
Noah Friedman <speelbarrow@speely.net>
2025-06-28 20:26:31 -0400

    modified: .config/nix/nixos.nix

--- .config/nix/nixos.nix
+++ .config/nix/nixos.nix
@@ -1,19 +1,9 @@
 { lib, ... }: lib.optionalAttrs (builtins.pathExists "/etc/nixos/configuration.nix") {
-  fileSystems = {
-    "/rosetta" = {
-      device = "rosetta";
-      fsType = "virtiofs";
-      noCheck = true;
-      options = [
-        "ro"
-        "nofail"
-      ];
-    };
-  };
   services = {
     xserver = {
       desktopManager.pantheon.enable = true;
       enable = true;
     };
   };
+  virtualisation.rosetta.enable = true;
 }
