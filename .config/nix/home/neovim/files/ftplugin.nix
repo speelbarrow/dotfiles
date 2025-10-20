c25bf62fe81777b09c2359e217da74130824c385
Noah Friedman <speelbarrow@speely.net>
2025-10-20 15:05:08 -0400

    modified: .config/nix/home/neovim/files/ftplugin.nix

--- .config/nix/home/neovim/files/ftplugin.nix
+++ .config/nix/home/neovim/files/ftplugin.nix
@@ -1,76 +1,34 @@
 # TODO: Make this better
 { ... }: let
-  javascript = {
-    text = ''
-      vim.o.shiftwidth = 2
-      vim.o.softtabstop = 2
-      vim.o.tabstop = 2
-    '';
-  };
+  make = offset: ''
+    vim.o.shiftwidth = ${offset}
+    vim.o.softtabstop = ${offset}
+    vim.o.tabstop = ${offset}
+  '';
+  two = make "2";
+  four = make "4";
+  eight = make "8";
 in {
-  "after/ftplugin/arduino.lua".text = ''
-    vim.o.shiftwidth = 4
-    vim.o.softtabstop = 4
-    vim.o.tabstop = 4
+  "after/ftplugin/arduino.lua".text = four + ''
     vim.o.textwidth = 120
   '';
-  "after/ftplugin/asm.lua".text = ''
-    vim.o.shiftwidth = 8
-    vim.o.softtabstop = 8
-    vim.o.tabstop = 8
-  '';
+  "after/ftplugin/asm.lua".text = eight;
   "after/ftplugin/c.lua".text = "vim.o.textwidth = 120";
-  "after/ftplugin/cmake.lua".text = ''
-    vim.o.shiftwidth = 2
-    vim.o.softtabstop = 2
-    vim.o.tabstop = 2
-  '';
+  "after/ftplugin/cmake.lua".text = two;
   "after/ftplugin/cpp.lua".text = "vim.o.textwidth = 120";
-  "after/ftplugin/css.lua".text = ''
-    vim.o.shiftwidth = 2
-    vim.o.softtabstop = 2
-    vim.o.tabstop = 2
-  '';
-  "after/ftplugin/html.lua".text = ''
-    vim.o.shiftwidth = 2
-    vim.o.softtabstop = 2
-    vim.o.tabstop = 2
-  '';
-  "after/ftplugin/markdown.lua".text = ''
-    vim.o.shiftwidth = 2
-    vim.o.softtabstop = 2
-    vim.o.tabstop = 2
-  '';
-  "after/ftplugin/nix.lua".text = ''
-    vim.o.shiftwidth = 2
-    vim.o.softtabstop = 2
-    vim.o.tabstop = 2
-  '';
-  "after/ftplugin/bindzone.lua".text = ''
-    vim.o.shiftwidth = 8
-    vim.o.softtabstop = 8
-    vim.o.tabstop = 8
-  '';
-  "after/ftplugin/lua.lua".text = ''
-    vim.o.shiftwidth=2
-    vim.o.softtabstop=2
-    vim.o.tabstop=2
-  '';
-  "after/ftplugin/javascript.lua" = javascript;
-  "after/ftplugin/sh.lua".text = ''
-    vim.o.shiftwidth = 8
-    vim.o.softtabstop = 8
-    vim.o.tabstop = 8
-  '';
-  "after/ftplugin/sql.lua".text = ''
-    vim.o.shiftwidth = 2
-    vim.o.softtabstop = 2
-    vim.o.tabstop = 2
-  '';
-  "after/ftplugin/toml.lua".text = ''
-    vim.o.shiftwidth = 2
-    vim.o.softtabstop = 2
-    vim.o.tabstop = 2
-  '';
-  "after/ftplugin/typescript.lua" = javascript;
+  "after/ftplugin/cs.lua".text = four;
+  "after/ftplugin/css.lua".text = two; 
+  "after/ftplugin/cucumber.lua".text = two;
+  "after/ftplugin/glsl.lua".text = four;
+  "after/ftplugin/html.lua".text = two;
+  "after/ftplugin/markdown.lua".text = two;
+  "after/ftplugin/nix.lua".text = two;
+  "after/ftplugin/bindzone.lua".text = eight;
+  "after/ftplugin/lua.lua".text = two;
+  "after/ftplugin/javascript.lua".text = two;
+  "after/ftplugin/json.lua".text = two;
+  "after/ftplugin/sh.lua".text = eight;
+  "after/ftplugin/sql.lua".text = two;
+  "after/ftplugin/toml.lua".text = two;
+  "after/ftplugin/typescript.lua".text = two;
 }
